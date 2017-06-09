d4da0417650dbde24d14a7c67080151b7231c7c1
Yanick Lafontaine <yanicklafontaine@gmail.com>
2017-06-09 12:33:25 -0400

    Account creation.

--- index.php
+++ index.php
@@ -11,30 +11,41 @@ use BladeBTC\Helpers\WebHook;
 use BladeBTC\WebHookHandler;
 use Telegram\Bot\Api;
 
-
-/**
- * Load .env file
- */
-$dotenv = new Dotenv\Dotenv(__DIR__);
-$dotenv->load();
-
-/**
- * Connect Telegram API
- */
-$telegram = new Api(getenv('APP_ID'));
+try {
 
 
-/**
- * Set WebHookURL
- */
-if (isset($_GET['setWebHookUrl']) && !empty($_GET['setWebHookUrl'])) {
-    WebHook::set($telegram, $_GET['setWebHookUrl']);
+    /**
+     * Load .env file
+     */
+    $dotenv = new Dotenv\Dotenv(__DIR__);
+    $dotenv->load();
+
+    /**
+     * Connect Telegram API
+     */
+    $telegram = new Api(getenv('APP_ID'));
+
+
+    /**
+     * Set WebHookURL
+     */
+    if (isset($_GET['setWebHookUrl']) && !empty($_GET['setWebHookUrl'])) {
+        WebHook::set($telegram, $_GET['setWebHookUrl']);
+    }
+
+
+    /**
+     * WebHookHandler
+     */
+    $webHook = new WebHookHandler($telegram);
+
+} catch (Exception $e) {
+
+    if (getenv("DEBUG") == true) {
+        $telegram->sendMessage([
+            'test' => $e->getMessage(),
+        ]);
+    }
 }
-
-
-/**
- * WebHookHandler
- */
-$webHook = new WebHookHandler($telegram);
 
 
