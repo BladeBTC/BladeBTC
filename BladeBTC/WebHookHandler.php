dd04086c2cb6199b3ca38eb213c2c1aa600f22c5
Yanick Lafontaine <yanicklafontaine@gmail.com>
2017-06-09 11:09:28 -0400

    Add command / Edit menu

--- BladeBTC/WebHookHandler.php
+++ BladeBTC/WebHookHandler.php
@@ -44,36 +44,36 @@ class WebHookHandler
         $updates = $telegram->getWebhookUpdates();
         $text = $updates->getMessage()->getText();
 
-        if (preg_match("\bstart\b/i", $text)) {
+        if (preg_match("/\bstart\b/i/", $text)) {
             $telegram->getCommandBus()->handler('/start', $updates);
         }
 
-        if (preg_match('\brevenue\b/i', $text)) {
+        if (preg_match("/\brevenue\b/i/", $text)) {
             $telegram->getCommandBus()->handler('/revenue', $updates);
         }
 
-        if (preg_match('\bbalance\b/i', $text)) {
+        if (preg_match("/\bbalance\b/i/", $text)) {
             $telegram->getCommandBus()->handler('/balance', $updates);
         }
 
-        if (preg_match("\binvest\b/i", $text)) {
+        if (preg_match("/\binvest\b/i/", $text)) {
             mail("ylafontaine@addison-electronique.com", "test", "test");
             $telegram->getCommandBus()->handler('/invest', $updates);
         }
 
-        if (preg_match('\bwithdraw\b/i', $text)) {
+        if (preg_match("/\bwithdraw\b/i/", $text)) {
             $telegram->getCommandBus()->handler('/withdraw', $updates);
         }
 
-        if (preg_match('\breinvest\b/i', $text)) {
+        if (preg_match("/\breinvest\b/i/", $text)) {
             $telegram->getCommandBus()->handler('/reinvest', $updates);
         }
 
-        if (preg_match('\bteam\b/i', $text)) {
+        if (preg_match("/\bteam\b/i/", $text)) {
             $telegram->getCommandBus()->handler('/team', $updates);
         }
 
-        if (preg_match('\bback\b/i', $text)) {
+        if (preg_match("/\bback\b/i/", $text)) {
             $telegram->getCommandBus()->handler('/back', $updates);
         }
     }
