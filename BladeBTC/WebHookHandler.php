b35e018e77bb617ce854d1f36ff18aa82292ce97
Yanick Lafontaine <yanicklafontaine@gmail.com>
2017-06-09 09:28:46 -0400

    Modification des menus.

--- BladeBTC/WebHookHandler.php
+++ BladeBTC/WebHookHandler.php
@@ -29,17 +29,42 @@ class WebHookHandler
         /**
          * Handle commands
          */
-        $telegram->commandsHandler(true);
+        $update = $telegram->commandsHandler(true);
 
 
         /**
          * Handle text command with unicode characters (Button)
          */
-        $message = $telegram->getWebhookUpdates()->getMessage();
-        if ($message !== null && $message->has('text')) {
-            $command = "/" . strtolower(explode(" ", $message->getText())[0]);
-            $telegram->getCommandBus()->handler($command, $telegram->getWebhookUpdates());
+        if (preg_match('%start%', $update->getMessage()->getText())) {
+            $telegram->getCommandBus()->execute('start', '', $update);
         }
 
+        if (preg_match('%revenue%', $update->getMessage()->getText())) {
+            $telegram->getCommandBus()->execute('revenue', '', $update);
+        }
+
+        if (preg_match('%balance%', $update->getMessage()->getText())) {
+            $telegram->getCommandBus()->execute('balance', '', $update);
+        }
+
+        if (preg_match('%invest%', $update->getMessage()->getText())) {
+            $telegram->getCommandBus()->execute('invest', '', $update);
+        }
+
+        if (preg_match('%withdraw%', $update->getMessage()->getText())) {
+            $telegram->getCommandBus()->execute('withdraw', '', $update);
+        }
+
+        if (preg_match('%reinvest%', $update->getMessage()->getText())) {
+            $telegram->getCommandBus()->execute('reinvest', '', $update);
+        }
+
+        if (preg_match('%team%', $update->getMessage()->getText())) {
+            $telegram->getCommandBus()->execute('team', '', $update);
+        }
+
+        if (preg_match('%back%', $update->getMessage()->getText())) {
+            $telegram->getCommandBus()->execute('back', '', $update);
+        }
     }
 }
