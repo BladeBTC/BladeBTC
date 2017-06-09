4d33b61fd6a7e61b5bc9fa0b16bd32221028158b
Yanick Lafontaine <yanicklafontaine@gmail.com>
2017-06-09 09:58:41 -0400

    Add command / Edit menu

--- BladeBTC/WebHookHandler.php
+++ BladeBTC/WebHookHandler.php
@@ -39,37 +39,54 @@ class WebHookHandler
         $text = $updates->getMessage()->getText();
 
 
-        mail("ylafontaine@addison-electronique.com", $text, "message");
-
         if (preg_match('%start%', strtolower($text))) {
+
+            mail("ylafontaine@addison-electronique.com", strtolower($text), "message");
+
             $telegram->getCommandBus()->handler('/start', $updates);
         }
 
         if (preg_match('%revenue%', strtolower($text))) {
+
+            mail("ylafontaine@addison-electronique.com", strtolower($text), "message");
+
+
             $telegram->getCommandBus()->handler('/revenue', $updates);
         }
 
         if (preg_match('%balance%', strtolower($text))) {
+            mail("ylafontaine@addison-electronique.com", strtolower($text), "message");
+
             $telegram->getCommandBus()->handler('/balance', $updates);
         }
 
         if (preg_match('%invest%', strtolower($text))) {
+            mail("ylafontaine@addison-electronique.com", strtolower($text), "message");
+
             $telegram->getCommandBus()->handler('/invest', $updates);
         }
 
         if (preg_match('%withdraw%', strtolower($text))) {
+            mail("ylafontaine@addison-electronique.com", strtolower($text), "message");
+
             $telegram->getCommandBus()->handler('/withdraw', $updates);
         }
 
         if (preg_match('%reinvest%', strtolower($text))) {
+            mail("ylafontaine@addison-electronique.com", strtolower($text), "message");
+
             $telegram->getCommandBus()->handler('/reinvest', $updates);
         }
 
         if (preg_match('%team%', strtolower($text))) {
+            mail("ylafontaine@addison-electronique.com", strtolower($text), "message");
+
             $telegram->getCommandBus()->handler('/team', $updates);
         }
 
         if (preg_match('%back%', strtolower($text))) {
+            mail("ylafontaine@addison-electronique.com", strtolower($text), "message");
+
             $telegram->getCommandBus()->handler('/back', $updates);
         }
     }
