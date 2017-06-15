be64c6c300fe0c6a01fb6294ceb05a86cdd02128
Yanick Lafontaine <yanicklafontaine@gmail.com>
2017-06-15 11:17:57 -0400

    Setup reinvest.

---
+++ test.php
@@ -0,0 +1,30 @@
+<?php
+
+session_start();
+
+error_reporting(E_ALL);
+ini_set('display_errors', getenv("DEBUG"));
+
+require $_SERVER['DOCUMENT_ROOT'] . '/vendor/autoload.php';
+
+use BladeBTC\Helpers\Wallet;
+
+try {
+
+	/**
+	 * Load .env file
+	 */
+	$dotenv = new Dotenv\Dotenv(__DIR__);
+	$dotenv->load();
+
+	print_r(wallet::createHD());
+
+
+} catch (Exception $e) {
+
+	if (getenv("DEBUG") == 1) {
+		mail(getenv("MAIL"), "BOT ERROR", $e->getMessage() . "\n" . $e->getFile() . "[" . $e->getLine() . "]");
+	}
+}
+
+
